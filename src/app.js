09923552ac729ead01bafa8ac984d2605fd191d6
Mareddy Venkateswara Reddy <mareddy.reddy@wipro.com>
2018-03-26 19:38:13 +0530

    expensify-app with redux

--- src/app.js
+++ src/app.js
@@ -7,22 +7,20 @@ import getVisibleExpenses from "./selectors/expenses";
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-import { addExpense, removeExpense, editExpense } from './actions/expenses';
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from "./actions/filters";
+import { addExpense} from './actions/expenses';
+import { setTextFilter} from "./actions/filters";
 
 const store = configureStore();
 
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-});
 
 const expenseOne = store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }));
 const expenseTwo = store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1500 }));
 const expenseThree = store.dispatch(addExpense({ description: 'rent', amount: 109500 }));
 // store.dispatch(setTextFilter('water'));
 
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
